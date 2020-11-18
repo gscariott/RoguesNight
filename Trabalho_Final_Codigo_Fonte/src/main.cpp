b9664f58785eee4398cce1ad31cb8f354537b150
Guilherme Scariott <gmscariott@gmail.com>
2020-11-18 11:32:50 -0300

    Compiler bug fix v.1

--- Trabalho_Final_Codigo_Fonte/src/main.cpp
+++ Trabalho_Final_Codigo_Fonte/src/main.cpp
@@ -458,7 +458,7 @@ int main(int argc, char* argv[])
         DrawVirtualObject("plane");
 
         model = Matrix_Translate(0.0f,-0.5f,0.0f)
-              * Matrix_Scale(5.0f,15.0f,5.0f);
+              * Matrix_Scale(5.0f,10.0f,5.0f);
         glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
         glUniform1i(object_id_uniform, WALL);
         DrawVirtualObject("wall");
